068fc3ee99dc4b1d81fae02c1b6014f6c99d517f
Benjamin Casalino <benjamincasalino@Benjamins-MacBook-Pro.local>
2015-09-05 19:41:44 -0700

    update paths

--- login.php
+++ login.php
@@ -1,36 +1,18 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
-
     <title>Test-PHP</title>
-
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <!-- Bootstrap theme -->
-
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
-
   <body role="document">
-
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top"><!-- Fixed navbar -->
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -39,24 +21,19 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">App Name</a>
+          <a class="navbar-brand" href="index.php">App Name</a>
             <nav class="nav navbar-nav navbar-right">
-
                 <form class="navbar-form navbar-nav nav navbar-right" role="search">
                     <div class="form-group">
-
                       <input type="text" class="form-control" placeholder="Username">
                       <input type="text" class="form-control" placeholder="Password">
-                      <button type="submit" class="btn btn-info"><a href="login.php">Login</a></button>
-                      <button type="button" class="btn btn-primary"><a href="signup.php">Sign Up </a></button>
+                      <a href="login.php"><button type="button" class="btn btn-info">Login </button></a>
+                      <a href="signup.php"><button type="button" class="btn btn-primary">Sign Up </button></a>
+                    </div>
                   </form>
-
-
-
-
-        </div>    </div>
-        </div>
-    </nav>
+              </div>
+             </div>
+            </nav>
 
 
 <br><br><br><br><br>
--- standings.php
+++ standings.php
@@ -1,36 +1,18 @@
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
-
-    <title>Theme Template for Bootstrap</title>
-
+    <title>Test-PHP</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
-    <!-- Bootstrap theme -->
-
-
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
     <script src="../../assets/js/ie-emulation-modes-warning.js"></script>
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
-
   <body role="document">
-
-    <!-- Fixed navbar -->
-    <nav class="navbar navbar-inverse navbar-fixed-top">
+    <nav class="navbar navbar-inverse navbar-fixed-top"><!-- Fixed navbar -->
       <div class="container">
         <div class="navbar-header">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
@@ -39,20 +21,24 @@
             <span class="icon-bar"></span>
             <span class="icon-bar"></span>
           </button>
-          <a class="navbar-brand" href="#">App Name</a>
-        </div>
-        <div id="navbar" class="navbar-collapse collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li class="login"><a href="#">Log In</a></li>
-            <li><a href="signup">Sign Up</a></li>
+          <a class="navbar-brand" href="index.php">App Name</a>
+            <nav class="nav navbar-nav navbar-right">
+                <form class="navbar-form navbar-nav nav navbar-right" role="search">
+                    <div class="form-group">
+                      <input type="text" class="form-control" placeholder="Username">
+                      <input type="text" class="form-control" placeholder="Password">
+                      <a href="login.php"><button type="button" class="btn btn-info">Login </button></a>
+                      <a href="signup.php"><button type="button" class="btn btn-primary">Sign Up </button></a>
+                    </div>
+                  </form>
+              </div>
+             </div>
+            </nav>
 
-              </ul>
-            </li>
-          </ul>
-        </div><!--/.nav-collapse -->
-      </div>
-    </nav>
 
+
+
+            
 <br><br><br><br><br>
 <body>
 
